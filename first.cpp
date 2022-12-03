eb237611e20fe59426b89cbd47f5334411877212
albez <albez2006@yandex.ru>
2022-12-03 10:59:44 +0300

    чему вы научились

--- first.cpp
+++ first.cpp
@@ -1,5 +1,20 @@
+#include <iostream>
+using namespace std;
+
 int main()
 {
-	std::cout >> "niggers";
+//	cout << "Hello World!\n";
+	string name;
+	cout << "Õòî?\t";
+	cin >> name;
+	cout << "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUGH, " << name << ".\n";
+	
+	string triangle = "";
+	for (int i = 0; i < 50; i++) {
+		cout << triangle << "\\\n";
+		triangle += "[";
+	}
 	return 0;
 }
+
+
